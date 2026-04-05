e15e0d5835f9d07209212508b576c942750f39ce
Nassi Harel <nassiharel@microsoft.com>
2026-04-05 17:55:30 +0300

    improve TUI

---
+++ internal/cli/tools.go
@@ -0,0 +1,59 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+
+	"github.com/nassiharel/clim/internal/registry"
+)
+
+var toolsCmd = &cobra.Command{
+	Use:   "tools",
+	Short: "Manage the tool definitions",
+}
+
+var toolsPathCmd = &cobra.Command{
+	Use:   "path",
+	Short: "Print the path to tools.yaml",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path, err := registry.ToolsPath()
+		if err != nil {
+			return err
+		}
+		fmt.Println(path)
+		return nil
+	},
+}
+
+var toolsEditCmd = &cobra.Command{
+	Use:   "edit",
+	Short: "Open tools.yaml in your editor",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path, err := registry.ToolsPath()
+		if err != nil {
+			return err
+		}
+
+		editor := os.Getenv("EDITOR")
+		if editor == "" {
+			editor = os.Getenv("VISUAL")
+		}
+		if editor == "" {
+			return fmt.Errorf("no $EDITOR set; edit %s manually", path)
+		}
+
+		c := exec.Command(editor, path)
+		c.Stdin = os.Stdin
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		return c.Run()
+	},
+}
+
+func init() {
+	toolsCmd.AddCommand(toolsPathCmd)
+	toolsCmd.AddCommand(toolsEditCmd)
+}
